79e51d7a9bdf8570cb476ce3a7f18d2e7e8c8f84
JCWoodworker <jc@rilocalwoodworks.com>
2023-08-01 06:50:41 -0400

    updated some scss

--- src/components/main_page/MainPage.tsx
+++ src/components/main_page/MainPage.tsx
@@ -2,8 +2,19 @@ import "./mainPage.scss"
 
 const MainPage = () => {
 	return (
-		<div className="main-page">
-			<h1>Main Page</h1>
+		<div className="main-page-container">
+			<div>
+				<h1>Main Page</h1>
+			</div>
+			<div>
+				<h1>Div 2</h1>
+			</div>
+			<div>
+				<h1>Div 3</h1>
+			</div>
+			<div>
+				<h1>Div 4</h1>
+			</div>
 		</div>
 	)
 }
