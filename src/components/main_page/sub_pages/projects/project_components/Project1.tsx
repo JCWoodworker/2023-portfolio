60de2f34f8ad3187f7cbe157954c00876157f43b
JCWoodworker <jc@rilocalwoodworks.com>
2023-08-05 21:18:29 -0400

    changed the a tag in project 1

--- src/components/main_page/sub_pages/projects/project_components/Project1.tsx
+++ src/components/main_page/sub_pages/projects/project_components/Project1.tsx
@@ -1,14 +1,17 @@
 const Project1: React.FC = () => {
 	return (
 		<div className="project-card project-1">
-			<a href="https://woodworking-project-calc.herokuapp.com/" target="_blank">
-				<h1>Woodworker's Project Manager</h1>
-				<p>React - Node - Express - PostgreSQL</p>
-				<p>Deployed with Heroku</p>
-				<p>
-					Sandbox Logins: user1@test.com or admin@test.com - Passwords for both are "test"{" "}
-				</p>
-			</a>
+			<h1>Woodworker's Project Manager</h1>
+			<p>React - Node - Express - PostgreSQL</p>
+			<p>Deployed with Heroku</p>
+			<p>
+				Sandbox Logins: user1@test.com or admin@test.com - Passwords for both
+				are "test"
+			</p>
+			<a
+				href="https://woodworking-project-calc.herokuapp.com/"
+				target="_blank"
+			/>
 		</div>
 	)
 }
