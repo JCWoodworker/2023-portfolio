bc11b00274491647fe8f1fe4651413163b6225ff
JCWoodworker <jc@rilocalwoodworks.com>
2023-11-12 23:04:07 -0500

    more styling updates and FIXES

--- src/components/main_page/sub_pages/background/Background.tsx
+++ src/components/main_page/sub_pages/background/Background.tsx
@@ -7,9 +7,22 @@ const Home: React.FC = () => {
 				<h1 className="sub-page-h1">About Me</h1>
 			</div>
 			<div className="background-page-container">
-				<p className="background-page-current-role">Operations Engineer @ Grubhub - Boston, MA - March 2022 to Present</p>
-				<p className="background-page-roles">Software Engineer | Woodworker | Father & Dog-Dad</p>
-				<p className="background-page-accomplishments"><span id="accomplishments">Accomplishments at Grubhub:</span> Built desktop app using Electron & React to handle bulk data operations and restaurant updates -  Built repo of python automations saving HOURS of manual work</p>
+			<ul className="background-page-roles">
+					<li>Software Engineer</li>
+					<li>Woodworker</li>
+					<li>Father & Dog-Dad</li>
+				</ul>
+
+				<p className="background-page-current-role">
+					Operations Engineer @ Grubhub - Boston, MA - March 2022 to Present
+				</p>
+				<ul className="background-page-accomplishments">Accomplishments at Grubhub:
+					<li>
+						Built desktop app using Electron & React to handle bulk data
+						operations and restaurant updates
+					</li>
+					<li>Built repo of python automations saving HOURS of manual work</li>
+				</ul>
 			</div>
 		</div>
 	)
