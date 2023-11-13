e28569c0e41bb489915a36576ae0fdb70cafbc3c
JCWoodworker <jc@rilocalwoodworks.com>
2023-11-12 23:43:00 -0500

    updated skills ui

--- src/components/main_page/sub_pages/skills/Skills.tsx
+++ src/components/main_page/sub_pages/skills/Skills.tsx
@@ -8,12 +8,23 @@ const Skills: React.FC = () => {
 			<div className="sub-page-header-div">
 				<h1 className="sub-page-h1">What Technologies have I worked with?</h1>
 			</div>
+			<ul className="skill-level-key">Skill Level Key:
+				<li>{`| Highly Skilled |`}</li>
+				<li>{`| Proficient |`}</li>
+				<li>{`| Normal |`}</li>
+			</ul>
 			<div className="skills-page-container">
 				{skillsArray.map((skill, index) => {
-					if (skill[1] === true) {
+					if (skill[1] === true && skill[2] === true) {
 						return (
 							<div key={index}>
-								{typedWordAnimation(skill[0], "skill-text highlighted-skills")}
+								{typedWordAnimation(skill[0], "skill-text bold-skill highlighted-skill")}
+							</div>
+						)
+					} else if (skill[1] === true && skill[2] === false) {
+						return (
+							<div key={index}>
+								{typedWordAnimation(skill[0], "skill-text bold-skill")}
 							</div>
 						)
 					}
